ce09a568bd4531b29a7199a06a1b4a8f83c0fc77
MxDn <12962232+MxDn@users.noreply.github.com>
2022-01-17 05:00:57 +0100

    Update EcollabFieldSanitizer.java

--- EcollabFieldSanitizer.java
+++ EcollabFieldSanitizer.java
@@ -1,15 +1,19 @@
-package com.mdm.accounting.batch.marketplace.annualtaxreturn.utils;
+package com.mdm.accounting.batch.marketplace.annualtaxreturn.utils; 
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
-import net.logstash.logback.encoder.org.apache.commons.lang3.RegExUtils;
 
 /**
  * @author dejardin-m
@@ -17,198 +21,96 @@ import net.logstash.logback.encoder.org.apache.commons.lang3.RegExUtils;
  */
 @Slf4j
 public final class EcollabFieldSanitizer {
-	abstract class FieldSanitizer {
 
-		/**
-		 * Regex for unprintable char
-		 */
-		private static final String UNPRINTABLES = "\\p{C}";
-		private static final String disallowedCharsPatternForSellerAndAdress = "[!#$%&()*+,\\/:;<=>?\\[\\\\\\\\]@^_`{|}~¡¢£¤¥¦§¨©ª¬®¯°±²³´µ¶·¸¹º¼½¾¿ÆÐ×Ø×ØÞßæð÷øþ]";
-		private final Pattern replacementPattern;
+	private static final String OTHER_FIELDNAME = "Other";
+	private static final Pattern unprintablePattern = Pattern.compile("\\p{C}");
+	private static final Pattern disallowedCharsRegex = Pattern
+			.compile("[\\\\!#$%&()*+,\\/:;<=>?\\[\\]@^_`{|}~¡¢£¤¥¦§¨©ª¬®¯°±²³´µ¶·¸¹º¼½¾¿ÆÐ×ØÞßæð÷øþ]");
 
-		/**
-		 * @param disallowedChars : Arrays of char that must be replaced
-		 */
-		FieldSanitizer(final String disallowedCharsPattern) {
-			this.replacementPattern = Pattern.compile(disallowedCharsPattern);
-		}
-
-		/**
-		 * @param value
-		 * @return
-		 */
-		private String replaceByRegex(String value) {
-			return this.replacementPattern.matcher(value).replaceAll(StringUtils.EMPTY);
-		}
-
-		private String sanitize(String value) {
-			if (StringUtils.isBlank(value)) {
-				return StringUtils.EMPTY;
-			}
-
-			// Order can have can change the returned value
-
-			value = StringUtils.trim(value);
-
-			value = RegExUtils.replaceAll(value, FieldSanitizer.UNPRINTABLES,
-					StringUtils.EMPTY);
-
-			value = specificSanitize(value);
-			value = replaceByRegex(value);
-			// value = replaceCharactersBySpace(value, this.disallowedChars);
-
-			value = StringUtils.stripAccents(value);
-
-			// Keep at end after all replacement
-			return StringUtils.normalizeSpace(value);
-
-		}
-
-		/**
-		 * Additional actions
-		 * @param value to be sanitize
-		 * @return sanitized value
-		 */
-		protected abstract String specificSanitize(String value);
-	}
-
-	class FieldSanitizerAddress extends FieldSanitizer {
-
-		private final char[] specificPositionnedChars = { '\'', ' ', '-', '.' };
-
-		FieldSanitizerAddress() {
-			this(EcollabFieldSanitizer.this.disallowedCharsPatternForSellerAndAdress);
-		}
-
-		FieldSanitizerAddress(final String disallowedCharsPattern) {
-			super(disallowedCharsPattern);
-		}
-
-		private String removeNotWellPositionnedCharacters(String value) {
-			if (StringUtils.isBlank(value)) {
-				return StringUtils.EMPTY;
-			}
-			while (ArrayUtils.contains(this.specificPositionnedChars,
-					value.charAt(0))) {
-				value = value.substring(1);
-			}
-			while (ArrayUtils.contains(this.specificPositionnedChars,
-					value.charAt(value.length() - 1))) {
-				value = value.substring(0, value.length() - 1);
-			}
-			return value;
-		}
-
-		@Override
-		protected String specificSanitize(String value) {
-			return removeNotWellPositionnedCharacters(value);
-		}
-	}
-
-	class FieldSanitizerDefault extends FieldSanitizer{
-		FieldSanitizerDefault() {
-			this(StringUtils.EMPTY);
-		}
-
-		FieldSanitizerDefault(final String disallowedCharsPattern) {
-			super(disallowedCharsPattern);
-		}
-
-		@Override
-		protected String specificSanitize(String value) {
-			return value;
-		}
-
-	}
-class FieldSanitizerSeller extends FieldSanitizer {
-
-	private final String[][] replacements = new String[][] {
+	private static final Pattern positionnedCharsRegex = Pattern.compile("([ \\\\\\-]{1})([ \\\\\\-.]{1})|([.]{1})([\\\\\\-]{1})|(^[ \\\\\\-.])|([ \\\\\\-.]$)");
+	  
+	private static final String[][] replacements = new String[][] {
 		// @formatter:off
 		{ "@", "a" },
 		{ "&", "et" }
 		// @formatter:on
 	};
 
-	FieldSanitizerSeller() {
-		this(EcollabFieldSanitizer.this.disallowedCharsPatternForSellerAndAdress);
-	}
+	
 
-	FieldSanitizerSeller(final String disallowedChars) {
-		super(disallowedChars);
-	}
+	private static final UnaryOperator<String> removeUnprintables = value -> unprintablePattern.matcher(value)
+			.replaceAll(StringUtils.EMPTY);
+	
+	private static final UnaryOperator<String> replaceDisallowedCharsBySpace = value -> disallowedCharsRegex
+			.matcher(value).replaceAll(StringUtils.EMPTY);
 
-	/**
-	 * use replacements tables to sanitize value
-	 * @param value to sanitize
-	 * @return sanitized value
-	 */
-	private String doReplacements(String value) {
+	private static final UnaryOperator<String> stripAccentsAndNormalizeSpace = value -> StringUtils
+			.normalizeSpace(StringUtils.stripAccents(value));
+
+	private static final UnaryOperator<String> doReplacements = value -> {
 		if (StringUtils.isBlank(value)) {
 			return StringUtils.EMPTY;
 		}
-		for (int i = 0; i < this.replacements.length; i++) {
-			value = StringUtils.replaceAll(value, this.replacements[i][0],
-					this.replacements[i][1]);
+		for (int i = 0; i < replacements.length; i++) {
+			value =Pattern.compile(replacements[i][0]).matcher(value).replaceAll(replacements[i][1]); 
 		}
 		return value;
-	}
+	};
 
-	@Override
-	protected String specificSanitize(String value) {
-		return doReplacements(value);
-	}
-}
-	private static final String OTHER_FIELDNAME = "Other";
+	private static final UnaryOperator<String> removeNotWellPositionnedCharacters = value -> { 
+		if (StringUtils.isBlank(value)) {
+			return StringUtils.EMPTY;
+		}
+		while (positionnedCharsRegex.matcher(value).find()) {
+			value = positionnedCharsRegex.matcher(value).replaceAll(StringUtils.SPACE);
+		}
+		return value;
+	};
 
-	public final String disallowedCharsPatternForSellerAndAdress = null;
+	private static final Function<String, String> sanitizingFunctionsDefault = stripAccentsAndNormalizeSpace
+			.compose(removeUnprintables);
 
-	/**
-	 * FieldSanitizer for address fields
-	 */
-	private final FieldSanitizer fieldSanitizerAddress = new FieldSanitizerAddress();
+	private static final Function<String, String> sanitizingFunctionsAddressFields = stripAccentsAndNormalizeSpace.compose(
+			removeNotWellPositionnedCharacters.compose(replaceDisallowedCharsBySpace.compose(removeUnprintables)));
 
-	/**
-	 * FieldSanitizer for seller fields
-	 */
-	private final FieldSanitizer fieldSanitizerSeller = new FieldSanitizerSeller();
+	private static final Function<String, String> sanitizingFunctionsSellerFields = stripAccentsAndNormalizeSpace
+			.compose(replaceDisallowedCharsBySpace.compose(doReplacements.compose(removeUnprintables)));
 
-	/**
-	 * FieldSanitizer for other fields
-	 */
-	private final FieldSanitizer fieldSanitizerByDefault = new FieldSanitizerDefault();
+	private final Map<String, Function<String, String>> sanitizingFunctionsByfieldNames;
 
-	private final Map<String, FieldSanitizer> sanitizersByfieldName;
+	public EcollabFieldSanitizer(final String[] sellerFieldNames, final String[] addressFieldNames) {
 
-	public EcollabFieldSanitizer(final String[] sellerFieldNames,
-			final String[] addressFieldNames) {
+		sanitizingFunctionsByfieldNames = new HashMap<>();
+		
+		Arrays.stream(sellerFieldNames).forEach(field -> this.sanitizingFunctionsByfieldNames.put(field,
+				EcollabFieldSanitizer.sanitizingFunctionsSellerFields));
 
-		this.sanitizersByfieldName = new HashMap<>();
-		Arrays.stream(sellerFieldNames).forEach(
-				field -> this.sanitizersByfieldName.put(field, this.fieldSanitizerSeller));
+		Arrays.stream(addressFieldNames).forEach(field -> this.sanitizingFunctionsByfieldNames.put(field,
+				EcollabFieldSanitizer.sanitizingFunctionsAddressFields));
 
-		Arrays.stream(addressFieldNames).forEach(
-				field -> this.sanitizersByfieldName.put(field, this.fieldSanitizerAddress));
+		this.sanitizingFunctionsByfieldNames.put(OTHER_FIELDNAME, EcollabFieldSanitizer.sanitizingFunctionsDefault);
 
-		this.sanitizersByfieldName.put(OTHER_FIELDNAME, this.fieldSanitizerByDefault);
 	}
 
 	/**
-	 * @param value to sanitize
+	 * @param value     to sanitize
 	 * @param fieldName
 	 * @return sanitized value
 	 */
 	public String sanitized(final String value, final String fieldName) {
-		var sanitizedValue = value;
-		if (this.sanitizersByfieldName.containsKey(fieldName)) {
-			sanitizedValue = this.sanitizersByfieldName.get(fieldName).sanitize(sanitizedValue);
+		if (StringUtils.isBlank(value)) {
+			return StringUtils.EMPTY;
 		}
-		else {
-			sanitizedValue = this.sanitizersByfieldName.get(OTHER_FIELDNAME).sanitize(value);
-		}
-
+		
+ 		var sanitizingFunctions = this.sanitizingFunctionsByfieldNames.get(OTHER_FIELDNAME);
+		if (this.sanitizingFunctionsByfieldNames.containsKey(fieldName)) {
+			sanitizingFunctions = this.sanitizingFunctionsByfieldNames.get(fieldName);
+		}  
+		
+		String sanitizedValue = sanitizingFunctions.apply(value);
 		if (!sanitizedValue.equals(value)) {
 
-			log.warn("{} : {} sanitized to {}", fieldName, value, sanitizedValue);
+			// log.warn("{} : {} sanitized to {}", fieldName, value, sanitizedValue);
 		}
 		return sanitizedValue;
 	}
